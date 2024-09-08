c3a058012347249705e81dff1e3d6da0213d5fc6
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-08 17:37:14 -0400

    press space to continue

--- Assets/Scripts/UIGameOver.cs
+++ Assets/Scripts/UIGameOver.cs
@@ -2,9 +2,19 @@
 
 public class UIGameOver : MonoBehaviour
 {
+    private bool _input;
     public void RestartLevel()
     {
         GameManager gameManager = GameManager.Instance;
         gameManager.RestartLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !_input)
+        {
+            RestartLevel();
+            _input = true;
+        }
     }
 }
--- Assets/Scripts/UILevelComplete.cs
+++ Assets/Scripts/UILevelComplete.cs
@@ -2,9 +2,19 @@
 
 internal class UILevelComplete : MonoBehaviour
 {
+    private bool _input;
     public void NextLevel()
     {
         GameManager gameManager = GameManager.Instance;
         gameManager.NextLevel();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Jump") && !_input)
+        {
+            NextLevel();
+            _input = true;
+        }
     }
 }
