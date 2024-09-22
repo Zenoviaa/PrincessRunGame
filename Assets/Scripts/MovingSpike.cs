906560c6410f017204485257e72d206ae731e599
Scolon <49799704+sscolon@users.noreply.github.com>
2024-09-22 19:44:11 -0400

    add cave assets

--- Assets/Scripts/MovingSpike.cs
+++ Assets/Scripts/MovingSpike.cs
@@ -1,12 +1,21 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 internal class MovingSpike : MonoBehaviour
 {
+    private float _swoopTimer;
+    private bool _goingUp;
     private Rigidbody2D _rigidbody;
     private Vector2 _velocity;
     public float movementSpeed;
     public bool snaptoGround;
+
     public LayerMask spikeLayer;
+
+
+    public bool swooping;
+    public float swoopMovementSpeed;
+    public float swoopDuration;
     private void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
@@ -23,7 +32,27 @@ internal class MovingSpike : MonoBehaviour
     private void FixedUpdate()
     {
         _velocity.x = movementSpeed;
+        if (swooping)
+        {
+            _swoopTimer += Time.fixedDeltaTime;
+            if(_swoopTimer >= swoopDuration)
+            {
+                _swoopTimer = 0;
+                _goingUp = !_goingUp;
+            }
+
+            if (_goingUp)
+            {
+                _velocity.y = Mathf.Lerp(_velocity.y, swoopMovementSpeed, Time.fixedDeltaTime);
+            }
+            else
+            {
+                _velocity.y = Mathf.Lerp(_velocity.y, -swoopMovementSpeed, Time.fixedDeltaTime);
+            }
+        }
+
         _rigidbody.velocity = _velocity;
+
 
         GameManager gameManager = GameManager.Instance;
         if (gameManager.EndedRun)
